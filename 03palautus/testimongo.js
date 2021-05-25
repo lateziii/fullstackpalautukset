d42b631515c2f09ad0a239c261d839933d35e5d0
lateziii <80420565+lateziii@users.noreply.github.com>
2021-05-25 13:00:00 +0300

    osa 3

---
+++ 03palautus/testimongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String, 
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name,
+  number
+})
+if (process.argv.length<4) {
+    console.log("phonebook: ")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+}
+else {
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`);
+        mongoose.connection.close()
+      })
+}
+
+
