e0d5dde300c9bfa402b29618a355f0be90e8daf5
lateziii <80420565+lateziii@users.noreply.github.com>
2021-06-12 15:46:39 +0300

    9.8-9.9

--- 09/patientor-backend/index.ts
+++ 09/patientor-backend/index.ts
@@ -2,7 +2,14 @@ import express from "express";
 import { Request, Response } from "express";
 
 const app = express();
+app.use(express.json)
 
-app.get("/ping", (_req: Request, res: Response) => {
+app.get("/api/ping", (_req: Request, res: Response) => {
     res.send("pong");
-  });
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
